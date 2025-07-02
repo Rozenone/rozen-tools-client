deb286d9324f0e9b0627052466e423c17dd2a5b6
rozenone <rozenone@foxmail.com>
2025-07-03 01:23:51 +0800

    代码改修

--- src-preload/preload.ts
+++ src-preload/preload.ts
@@ -1,1 +1,10 @@
 import './commonPreload.ts';
+import { contextBridge, ipcRenderer } from 'electron'
+
+contextBridge.exposeInMainWorld('electron', {
+  ipcRenderer: {
+    invoke: (...args: unknown[]) => ipcRenderer.invoke.apply(ipcRenderer, args),
+    on: (channel: string, listener: (...args: any[]) => void) => ipcRenderer.on(channel, listener),
+    // 可根据需要扩展更多方法
+  }
+})
